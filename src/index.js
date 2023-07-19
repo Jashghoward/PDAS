9c45e1af5ac6677a1afc092352ac9dcc6f0fccf6
Josh Howard <jashghoward@icloud.com>
2023-07-19 23:16:15 +0200

    index.js update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
 
+
+
 const el = document.getElementById('root')
 const root = ReactDom.createRoot(el);
 
