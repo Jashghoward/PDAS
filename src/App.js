29f91a8c5574a39afe14931bbbc974fdd8321b98
Josh Howard <jashghoward@icloud.com>
2023-07-19 23:17:36 +0200

    App.js updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
-
-
 console.log(AlexaImage);
 console.log(SiriImage);
 
